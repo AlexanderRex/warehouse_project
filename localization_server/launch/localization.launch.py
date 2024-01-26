695705fea2e8cd6fadfdfb2fe4208ab7062d763c
AlexanderRex <darkknightrises2018@gmail.com>
2024-01-26 10:58:58 +0000

    configuration created for sim and real, launch files fixed

--- localization_server/launch/localization.launch.py
+++ localization_server/launch/localization.launch.py
@@ -1,50 +1,50 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def get_file_path(package, subdir, filename):
+    return os.path.join(get_package_share_directory(package), subdir, filename)
 
 def generate_launch_description():
+    # declare launch arguments
+    declare_use_sim_time = DeclareLaunchArgument(name='use_sim_time', default_value='True',
+                                                 description='True: Use Simulation Clock & False: Use Real Robot Clock')
+    # use launch configuration in nodes
+    use_sim_time = LaunchConfiguration('use_sim_time')   # <--- you can use this use_sim_time into your node definitions
 
-    # Declare the launch arguments
-    map_file_arg = DeclareLaunchArgument(
-        'map_file',
-        default_value='warehouse_map_sim.yaml',
-        description='Map file to use for map server'
+    # get config files
+    amcl_sim = get_file_path("localization_server", "config", "amcl_config_sim.yaml")
+    amcl_real = get_file_path("localization_server", "config", "amcl_config_real.yaml")
+    map_sim = get_file_path("map_server", "config", "warehouse_map_sim.yaml")
+    map_real = get_file_path("map_server", "config", "warehouse_map_real.yaml")
+
+    # declare nodes
+    map_sim_node = Node(package="nav2_map_server", executable="map_server", name="map_server", output="screen",
+                        parameters=[{"use_sim_time": use_sim_time}, {"yaml_filename": map_sim}],
+                        condition=IfCondition(use_sim_time))
+    map_real_node = Node(package="nav2_map_server", executable="map_server", name="map_server", output="screen",
+                        parameters=[{"use_sim_time": use_sim_time}, {"yaml_filename": map_real}],
+                        condition=UnlessCondition(use_sim_time))
+
+    amcl_sim_node = Node(package="nav2_amcl", executable="amcl", name="amcl", output="screen",
+                        parameters=[amcl_sim], condition=IfCondition(use_sim_time))
+    amcl_real_node = Node(package="nav2_amcl", executable="amcl", name="amcl", output="screen",
+                        parameters=[amcl_real], condition=UnlessCondition(use_sim_time))
+
+    lifecycle_node = Node(package="nav2_lifecycle_manager", executable="lifecycle_manager", 
+                          name="lifecycle_manager_localization", output="screen",
+                          parameters=[{"use_sim_time": use_sim_time}, {"autostart": True},
+                                      {"node_names": ["map_server", "amcl"]}])
+
+
+    return LaunchDescription(
+        [declare_use_sim_time,
+         map_sim_node, map_real_node,
+         amcl_sim_node, amcl_real_node,
+         lifecycle_node
+        ]
     )
-
-    # Use LaunchConfiguration to dynamically get the map file
-    map_file = LaunchConfiguration('map_file')    
-
-    nav2_yaml = os.path.join(get_package_share_directory('localization_server'), 'config', 'amcl_config.yaml')
-
-    return LaunchDescription([
-        map_file_arg,
-        Node(
-            package='nav2_map_server',
-            executable='map_server',
-            name='map_server',
-            output='screen',
-            parameters=[{'use_sim_time': True}, 
-                        {'yaml_filename': [os.path.join(get_package_share_directory('map_server'), 'config'), '/', map_file]}]
-        ),
-            
-        Node(
-            package='nav2_amcl',
-            executable='amcl',
-            name='amcl',
-            output='screen',
-            parameters=[nav2_yaml]
-        ),
-
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_localization',
-            output='screen',
-            parameters=[{'use_sim_time': True},
-                        {'autostart': True},
-                        {'node_names': ['map_server', 'amcl']}]
-        )
-    ])
--- path_planner_server/launch/pathplanner.launch.py
+++ path_planner_server/launch/pathplanner.launch.py
@@ -1,61 +1,82 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def get_file_path(package, subdir, filename):
+    return os.path.join(get_package_share_directory(package), subdir, filename)
 
 def generate_launch_description():
 
-    controller_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'controller.yaml')
-    bt_navigator_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'bt.yaml')
-    planner_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'planner_server.yaml')
-    recovery_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'recovery.yaml')
-    rviz_config_file = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'pathplanning.rviz')
+     # declare launch arguments
+    declare_use_sim_time = DeclareLaunchArgument(name='use_sim_time', default_value='True',
+                                                 description='True: Use Simulation Clock & False: Use Real Robot Clock')
+    # use launch configuration in nodes
+    use_sim_time = LaunchConfiguration('use_sim_time')   # <--- you can use this use_sim_time into your node definitions
 
-    return LaunchDescription([     
-        Node(
-            package='nav2_controller',
-            executable='controller_server',
-            name='controller_server',
-            output='screen',
-            parameters=[controller_yaml],
-            remappings=[('/cmd_vel', '/robot/cmd_vel')]),
+    rviz_config_file = get_file_path("path_planner_server", "config", "pathplanning.rviz")
 
-        Node(
-            package='nav2_planner',
-            executable='planner_server',
-            name='planner_server',
-            output='screen',
-            parameters=[planner_yaml]),
-            
-        Node(
-            package='nav2_recoveries',
-            executable='recoveries_server',
-            name='recoveries_server',
-            parameters=[recovery_yaml],
-            output='screen'),
+    controller_yaml_real = get_file_path("path_planner_server", "config", "controller_real.yaml")
+    bt_navigator_yaml_real = get_file_path("path_planner_server", "config", "bt_real.yaml")
+    planner_yaml_real = get_file_path("path_planner_server", "config", "planner_server_real.yaml")
+    recovery_yaml_real = get_file_path("path_planner_server", "config", "recovery_real.yaml")
 
-        Node(
-            package='nav2_bt_navigator',
-            executable='bt_navigator',
-            name='bt_navigator',
-            output='screen',
-            parameters=[bt_navigator_yaml]),
+    controller_yaml_sim = get_file_path("path_planner_server", "config", "controller_sim.yaml")
+    bt_navigator_yaml_sim = get_file_path("path_planner_server", "config", "bt_sim.yaml")
+    planner_yaml_sim = get_file_path("path_planner_server", "config", "planner_server_sim.yaml")
+    recovery_yaml_sim = get_file_path("path_planner_server", "config", "recovery_sim.yaml")
 
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_pathplanner',
-            output='screen',
-            parameters=[{'autostart': True},
+    nav2_con_node_sim = Node(package='nav2_controller', executable='controller_server', name='controller_server',
+            output='screen', parameters=[controller_yaml_sim], remappings=[('/cmd_vel', '/robot/cmd_vel')],
+            condition=IfCondition(use_sim_time))
+    nav2_con_node_real = Node(package='nav2_controller', executable='controller_server', name='controller_server',
+            output='screen', parameters=[controller_yaml_real], remappings=[('/cmd_vel', '/robot/cmd_vel')],
+            condition=UnlessCondition(use_sim_time))
+
+    nav2_plan_node_sim = Node(package='nav2_planner', executable='planner_server', name='planner_server',
+            output='screen', parameters=[planner_yaml_sim],
+            condition=IfCondition(use_sim_time))
+    nav2_plan_node_real = Node(package='nav2_planner', executable='planner_server', name='planner_server',
+            output='screen', parameters=[planner_yaml_real],
+            condition=UnlessCondition(use_sim_time))
+
+    nav2_rec_sim = Node(package='nav2_recoveries', executable='recoveries_server', name='recoveries_server',
+            parameters=[recovery_yaml_sim], output='screen',
+            condition=IfCondition(use_sim_time))
+    nav2_rec_real = Node(package='nav2_recoveries', executable='recoveries_server', name='recoveries_server',
+            parameters=[recovery_yaml_real], output='screen',
+            condition=UnlessCondition(use_sim_time))
+
+    nav2_bt_nav_node_sim = Node(package='nav2_bt_navigator', executable='bt_navigator', name='bt_navigator',
+            output='screen', parameters=[bt_navigator_yaml_sim],
+            condition=IfCondition(use_sim_time))
+    nav2_bt_nav_node_real = Node(package='nav2_bt_navigator', executable='bt_navigator', name='bt_navigator',
+            output='screen', parameters=[bt_navigator_yaml_real],
+            condition=UnlessCondition(use_sim_time))
+    
+    nav2_life_man = Node(package='nav2_lifecycle_manager', executable='lifecycle_manager', name='lifecycle_manager_pathplanner',
+            output='screen', parameters=[{'autostart': True},
                         {'node_names': ['planner_server',
                                         'controller_server',
                                         'recoveries_server',
-                                        'bt_navigator']}]),
-
-        Node(
+                                        'bt_navigator']}])
+    rviz_node = Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
             output='screen',
             arguments=['-d', rviz_config_file])
-    ])
+
+    return LaunchDescription(
+        [declare_use_sim_time,
+        rviz_node,
+        nav2_con_node_sim, nav2_con_node_real,
+        nav2_plan_node_sim, nav2_plan_node_real,
+        nav2_rec_sim, nav2_rec_real,
+        nav2_bt_nav_node_sim, nav2_bt_nav_node_real,
+        nav2_life_man
+        ]
+    )
