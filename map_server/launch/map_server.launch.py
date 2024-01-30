9823a4509ca179ae28cb3a690266ecb8d6bb5abc
AlexanderRex <darkknightrises2018@gmail.com>
2024-01-30 18:38:36 +0000

    rviz launch added to map server launch, variable to change map added

--- map_server/launch/map_server.launch.py
+++ map_server/launch/map_server.launch.py
@@ -1,38 +1,50 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def get_file_path(package, subdir, filename):
+    return os.path.join(get_package_share_directory(package), subdir, filename)
 
 def generate_launch_description():
 
-    # Declare the launch arguments
-    map_file_arg = DeclareLaunchArgument(
-        'map_file',
-        default_value='warehouse_map_sim.yaml',
-        description='Map file to use for map server'
+    # declare launch arguments
+    declare_use_sim_time = DeclareLaunchArgument(name='use_sim_time', default_value='True',
+                                                 description='True: Use Simulation Clock & False: Use Real Robot Clock')
+    # use launch configuration in nodes
+    use_sim_time = LaunchConfiguration('use_sim_time')   # <--- you can use this use_sim_time into your node definitions
+
+    # get config files
+    map_sim = get_file_path("map_server", "config", "warehouse_map_sim.yaml")
+    map_real = get_file_path("map_server", "config", "warehouse_map_real.yaml")
+    rviz_config_file = get_file_path("map_server", "config", "mapper_rviz_config.rviz")
+
+    map_sim_node = Node(package="nav2_map_server", executable="map_server", name="map_server", output="screen",
+                        parameters=[{"use_sim_time": use_sim_time}, {"yaml_filename": map_sim}],
+                        condition=IfCondition(use_sim_time))
+    map_real_node = Node(package="nav2_map_server", executable="map_server", name="map_server", output="screen",
+                        parameters=[{"use_sim_time": use_sim_time}, {"yaml_filename": map_real}],
+                        condition=UnlessCondition(use_sim_time))
+
+    lifecycle_node = Node(package="nav2_lifecycle_manager", executable="lifecycle_manager", 
+                          name="lifecycle_manager", output="screen",
+                          parameters=[{"use_sim_time": use_sim_time}, {"autostart": True},
+                                      {"node_names": ["map_server"]}])
+
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_file])
+
+    return LaunchDescription(
+        [
+         declare_use_sim_time,
+         map_sim_node, map_real_node,
+         lifecycle_node, rviz_node
+        ]
     )
-
-    # Use LaunchConfiguration to dynamically get the map file
-    map_file = LaunchConfiguration('map_file')
-
-    return LaunchDescription([
-        map_file_arg,
-        Node(
-            package='nav2_map_server',
-            executable='map_server',
-            name='map_server',
-            output='screen',
-            parameters=[{'use_sim_time': True}, 
-                        {'yaml_filename': [os.path.join(get_package_share_directory('map_server'), 'config'), '/', map_file]}]),
-
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_mapper',
-            output='screen',
-            parameters=[{'use_sim_time': True},
-                        {'autostart': True},
-                        {'node_names': ['map_server']}])
-    ])
